1e06ce872c6dfa7ef83b695b08805ca4e3cbb89b
Jacob Ellis Duncan <jellisduncan@gmail.com>
2020-10-28 23:25:26 -0500

    slap a bot in here with only one command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
-// use std::env;
+use serenity::{
+    async_trait,
+    framework::StandardFramework,
+    model::gateway::{ Activity, Ready },
+    prelude::*,
+};
 
-mod bot;
+use serenity::framework::standard::macros::group;
 
-fn main() {
-    let bot = bot::Bot::new("token.", "!");
-    println!("token: {}, prefix: {}", bot.token, bot.prefix);
+use tracing::{error, info};
+
+mod commands;
+
+use dotenv::dotenv;
+use std::env;
+use commands::{
+    ping::*
+};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        ctx.set_activity(Activity::playing("What's my use?")).await;
+        info!("{} is online!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        error!("An error occured while running the client: {:?}", why);
+    }
 }
